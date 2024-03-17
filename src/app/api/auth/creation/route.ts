5d5c1cc9dc43a211216d6fcf24f4003278a492c8
osengprogrammer <147626933+osengprogrammer@users.noreply.github.com>
2024-03-17 09:20:39 -0700

    7th comment

--- src/app/api/auth/creation/route.ts
+++ src/app/api/auth/creation/route.ts
@@ -1,5 +1,5 @@
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
-// import prisma from "@/app/lib/db";
+import prisma from "@/lib/db";
 import { NextResponse } from "next/server";
 import { unstable_noStore as noStore } from "next/cache";
 
@@ -13,24 +13,24 @@ export async function GET() {
     throw new Error("Smoething went wrong, i am srorry....");
   }
 
-  // let dbUser = await prisma.user.findUnique({
-  //   where: {
-  //     id: user.id,
-  //   },
-  // });
+  let dbUser = await prisma.user.findUnique({
+    where: {
+      id: user.id,
+    },
+  });
 
-  // if (!dbUser) {
-  //   dbUser = await prisma.user.create({
-  //     data: {
-  //       email: user.email ?? "",
-  //       firstName: user.given_name ?? "",
-  //       lastName: user.family_name ?? "",
-  //       id: user.id,
-  //       profileImage:
-  //         user.picture ?? `https://avatar.vercel.sh/${user.given_name}`,
-  //     },
-  //   });
-  // }
+  if (!dbUser) {
+    dbUser = await prisma.user.create({
+      data: {
+        email: user.email ?? "",
+        firstName: user.given_name ?? "",
+        lastName: user.family_name ?? "",
+        id: user.id,
+        profileImage:
+          user.picture ?? `https://avatar.vercel.sh/${user.given_name}`,
+      },
+    });
+  }
 
-  return NextResponse.redirect("https://airbnb-yt-peach.vercel.app");
+  return NextResponse.redirect(`${process.env.KINDE_SITE_URL}`);
 }
