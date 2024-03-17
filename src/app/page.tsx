8f5d707c49be7c3c2746fbd39da693150f4adb64
osengprogrammer <147626933+osengprogrammer@users.noreply.github.com>
2024-03-17 05:43:16 -0700

    6th comment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
+import { MapFilterItems } from "@/components/custom/MapFilterItems";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-    Hello World
-  
+    <div className="container mx-auto px-5 lg:px-10">
+      <MapFilterItems />
     </div>
   );
 }
