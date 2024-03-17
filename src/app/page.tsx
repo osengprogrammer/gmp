8ad0be9e499ce3793d596fb291ec1a9bb218feb0
osengprogrammer <147626933+osengprogrammer@users.noreply.github.com>
2024-03-17 04:33:24 -0700

    third comment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <>
+    <div>
+    Hello World
   
-    </>
+    </div>
   );
 }
--- src/components/custom/Navbar.tsx
+++ src/components/custom/Navbar.tsx
@@ -1,20 +1,34 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 import DesktopLogo from "../../../public/airbnb-desktop.png"
+import { UserNav } from "./UserNav";
+
 function Navbar() {
   return (
-    <div className='w-full border-b border-green-500'>
-        <div className='flex items-center justify-between container mx-auto px-5 lg:px-10 py-5'>
+    <div className="w-full border-b border-green-500">
+      <div className="flex items-center justify-between container mx-auto px-5 lg:px-10 py-5">
+        
         <Link href="/">
-            <Image src={DesktopLogo} alt="Desktop Logo" className='w-32 hidden lg:block'/>
-            <Image src={DesktopLogo} alt="Desktop Logo" className='w-12 block lg:hidden'/>
+          <Image
+            src={DesktopLogo}
+            alt="Desktop Logo"
+            className="w-32 hidden lg:block"
+          />
+          <Image
+            src={DesktopLogo}
+            alt="Desktop Logo"
+            className="w-12 block lg:hidden"
+          />
         </Link>
-
+        <div className="rounded-full border px-5 py-5">
+          <p>Hello From Search</p>
         </div>
-       
+
+        <UserNav/>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
