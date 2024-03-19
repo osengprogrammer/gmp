4045cfed1cf95ac2e95240e576ed3dc5bbc8711e
osengprogrammer <147626933+osengprogrammer@users.noreply.github.com>
2024-03-19 05:51:12 -0700

    14th comment

--- src/components/custom/Navbar.tsx
+++ src/components/custom/Navbar.tsx
@@ -3,19 +3,17 @@ import Link from "next/link";
 import React from "react";
 import DesktopLogo from "../../../public/airbnb-desktop.png"
 import { UserNav } from "./UserNav";
+import SearchComponent from "./SearchComponent";
 
 function Navbar() {
   return (
     <div className="w-full border-b border-green-500">
-      <div className="flex items-center justify-between container mx-auto px-5 lg:px-10 py-5">
+      <div className="flex items-center justify-between container mx-auto ">
         
         <Link href="/">
-         <h1 className="text-green-800 text-4xl ">GMP</h1>
+         <h1 className="text-green-800 text-2xl lg:text-4xl">GMP</h1>
         </Link>
-        <div className="rounded-full border px-5 py-5">
-          <p>Hello From Search</p>
-        </div>
-
+        <SearchComponent/>
         <UserNav/>
       </div>
     </div>
